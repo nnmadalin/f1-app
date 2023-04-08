3027e8af127906e2f4295b41dfa191f111593f4a
nnmadalin <52752339+nnmadalin@users.noreply.github.com>
2023-04-08 15:47:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var jfile;
+
+$.getJSON('https://ergast.com/api/f1/2023/3/results.json', function(jfile) {
+   console.log(jfile);
+   var dir = jfile["MRData"]["RaceTable"]["Races"]["0"]["Results"];
+   console.log(jfile["MRData"]["RaceTable"]["Races"]["0"]["Results"])
+   for(let i = 19; i >= 0; i--){
+      var table = document.getElementById("race_results");
+      var row = table.insertRow(0);
+      var POS = row.insertCell(0);
+      var NO = row.insertCell(1);
+      var DRIVER = row.insertCell(2);
+      var CAR = row.insertCell(3);
+      var LAPS = row.insertCell(4);
+      var TIME = row.insertCell(5);
+      var PTS = row.insertCell(6);
+      POS.innerHTML = i + 1;
+      NO.innerHTML = dir[i]["number"]; 
+      DRIVER.innerHTML = dir[i]["Driver"]["givenName"] + " " + dir[i]["Driver"]["familyName"];
+      CAR.innerHTML = dir[i]["Constructor"]["name"];
+      LAPS.innerHTML = dir[i]["laps"];
+      if(dir[i]["Time"])
+         TIME.innerHTML = dir[i]["Time"]["time"];
+      else
+         TIME.innerHTML = "DNF";
+      PTS.innerHTML = dir[i]["points"];
+   }
+});
+
